fc900f6bb5ff573e151be2778b44e09dfac9307a
Onur <onatsurmeli@gmail.com>
2025-02-02 01:09:27 +0100

    Add tests for QuestionIndexView and QuestionDetailView to handle future and past questions

--- polls/tests.py
+++ polls/tests.py
@@ -2,7 +2,71 @@ import datetime
 
 from django.test import TestCase
 from django.utils import timezone
+from django.urls import reverse
 from .models import Question
+
+
+def create_question(question_text, days):
+    """
+    Create a question with the given `question_text` and published the given number of `days`
+    offset to now (negative for questions published in the past,
+    positive for questions that have yet to be published).
+    Args:
+        question_text (string): The text of the question.
+        days (int): The number of days to offset the question.
+
+    Returns:
+        Question: The created question.
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerySetEqual(response.context["latest_question_list"], [])
+
+    def test_past_question(self):
+        question = create_question(question_text="Past", days=-30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, question.question_text)
+        self.assertQuerySetEqual(response.context["latest_question_list"], [question])
+
+    def test_future_question(self):
+        question = create_question(question_text="Future", days=30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerySetEqual(response.context["latest_question_list"], [])
+
+    def test_future_question_and_past_question(self):
+        past_question = create_question(question_text="Past", days=-30)
+        future_question = create_question(question_text="Future", days=30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, past_question.question_text)
+        self.assertQuerySetEqual(
+            response.context["latest_question_list"], [past_question]
+        )
+
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        future_question = create_question(question_text="Future", days=5)
+        url = reverse("polls:detail", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        past_question = create_question(question_text="Past", days=-5)
+        url = reverse("polls:detail", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, past_question.question_text)
 
 
 class QuestionModelTests(TestCase):
@@ -18,7 +82,7 @@ class QuestionModelTests(TestCase):
         """
         was_published_recently() returns False for questions whose pub_date is older than 1 day.
         """
-        time =  timezone.now() + datetime.timedelta(days= -5)
+        time = timezone.now() + datetime.timedelta(days=-5)
         old_question = Question(pub_date=time)
         self.assertIs(old_question.was_published_recently(), False)
 
@@ -26,6 +90,6 @@ class QuestionModelTests(TestCase):
         """
         was_published_recently() returns True for questions whose pub_date is younger than 1 day.
         """
-        time =  timezone.now() + datetime.timedelta(days= -1, minutes=10)
+        time = timezone.now() + datetime.timedelta(days=-1, minutes=10)
         old_question = Question(pub_date=time)
         self.assertIs(old_question.was_published_recently(), True)
